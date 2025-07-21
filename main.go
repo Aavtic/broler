d9811f4f8fb83e781439b46291519dac5a80a201
Aadish M <89965681+Aavtic@users.noreply.github.com>
2025-07-21 22:39:55 +0530

    Added CLI

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"os"
+	"fmt"
 	"github.com/aavtic/broler/broler"
 )
 
+func usage() string {
+	return `
+usage: broler [-h | --help] [<url>]`
+}
+
 func main() {
-	broler.Run()
+	args := os.Args
+	if (len(args) < 2) {
+		fmt.Fprint(os.Stderr, "Not enough arguments\n", usage())
+		os.Exit(1)
+	}
+
+	if (args[1] == "-h" || args[1] == "--help") { fmt.Fprint(os.Stdout, "Not enough arguments\n", usage()) }
+
+	broler.Run(args[1])
 }
--- parsing/parsing.go
+++ parsing/parsing.go
@@ -1,13 +1,12 @@
 package parsing
 
 import (
-	"io"
 	"fmt"
+	"golang.org/x/net/html"
+	"io"
 	"slices"
 	"strings"
-	"golang.org/x/net/html"
 	// "golang.org/x/net/html/atom"
-
 	// "github.com/aavtic/broler/utils/queue"
 )
 
@@ -28,8 +27,8 @@ func TraverseNode(root *html.Node) {
 	// queue := queue.New()
 	for child := range root.Descendants() {
 		for _, attr := range child.Attr {
-			if (slices.Contains(URL_ATTRIBUTES, attr.Key)) {
-				fmt.Println(child.Data)
+			if slices.Contains(URL_ATTRIBUTES, attr.Key) {
+				fmt.Println(child.Data, ": ", attr.Val)
 			}
 		}
 	}
