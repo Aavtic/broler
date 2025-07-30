027713bfe11a07f45d6e0bfce4e0ffd7cb18bda4
Aadish M <89965681+Aavtic@users.noreply.github.com>
2025-07-30 09:41:34 +0530

    Updated CLI Args

--- main.go
+++ main.go
@@ -13,12 +13,18 @@ usage: broler [-h | --help] [<url>]`
 
 func main() {
 	args := os.Args
-	if (len(args) < 2) {
-		fmt.Fprint(os.Stderr, "Not enough arguments\n", usage())
+
+	if (len(args) == 1) {
+		fmt.Fprint(os.Stdout, usage())
 		os.Exit(1)
 	}
 
-	if (args[1] == "-h" || args[1] == "--help") { fmt.Fprint(os.Stdout, "Not enough arguments\n", usage()) }
+	if (args[1] == "-h" || args[1] == "--help") { fmt.Fprint(os.Stdout, usage()) }
+
+	if (len(args) != 2) {
+		fmt.Fprint(os.Stderr, "Not enough arguments\n", usage())
+		os.Exit(1)
+	}
 
 	broler.Run(args[1])
 }
