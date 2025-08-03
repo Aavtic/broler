ac53c22bb6ea71572f8b5a0421cfc7a1ae5961d8
Aadish M <89965681+Aavtic@users.noreply.github.com>
2025-08-04 00:09:42 +0530

    Added communication via channel between grpc and broler

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -10,20 +10,21 @@
 
 // package rpc;
 
-package rpc;
+package rpc
 
 import (
-	"io"
-	"net"
-	"log"
 	pb "github.com/aavtic/broler/rpc/broler_proto"
 	"google.golang.org/grpc"
+	"io"
+	"log"
+	"net"
 )
 
 type server struct {
 	pb.UnimplementedBrolerServer
 	data_chan chan *pb.Pages
 }
+
 // {
 //   "aavtic.dev": {
 //     is_end: false
@@ -37,25 +38,24 @@ func (s *server) PageInfo(req *pb.PagesInfoReq, stream pb.Broler_PageInfoServer)
 	for page := range s.data_chan {
 		log.Println(page)
 		if err := stream.Send(page); err != nil {
-			return err;
+			return err
 		}
-		log.Println("Sent stream");
+		log.Println("Sent stream")
 	}
-	return nil;
+	return nil
 }
 
-
 func (*server) ClientRequests(stream pb.Broler_ClientRequestsServer) error {
-	for ;; {
+	for {
 		client_request, err := stream.Recv()
 
 		if err == io.EOF {
-			return nil;
+			return nil
 		}
 
 		if err != nil {
 			return err
-		}	
+		}
 
 		log.Println("CLIENT REQUEST: ", client_request.Request)
 		stream.Send(&pb.ServerResponse{
@@ -66,80 +66,84 @@ func (*server) ClientRequests(stream pb.Broler_ClientRequestsServer) error {
 
 func RunServer(data_channel chan *pb.Pages) {
 	listener, err := net.Listen("tcp", ":50051")
-	if err != nil { log.Fatalf("Could not Listen due to %v", err) }
+	if err != nil {
+		log.Fatalf("Could not Listen due to %v", err)
+	}
 	log.Println("[+] Server up and running...")
-	
+
 	gRPC_server := grpc.NewServer()
 	pb.RegisterBrolerServer(gRPC_server, &server{data_chan: data_channel})
-	
+
 	if err := gRPC_server.Serve(listener); err != nil {
-		log.Fatalf("Could not server due to %v", err);
+		log.Fatalf("Could not server due to %v", err)
 	}
 }
 
 func main() {
 	listener, err := net.Listen("tcp", ":50051")
-	if err != nil { log.Fatalf("Could not Listen due to %v", err) }
+	if err != nil {
+		log.Fatalf("Could not Listen due to %v", err)
+	}
 	log.Println("[+] Server up and running...")
-	
+
 	gRPC_server := grpc.NewServer()
 	pb.RegisterBrolerServer(gRPC_server, &server{})
-	
+
 	if err := gRPC_server.Serve(listener); err != nil {
-		log.Fatalf("Could not server due to %v", err);
+		log.Fatalf("Could not server due to %v", err)
 	}
 }
 
 // EXAMPLE DATA
 
-	// pages := []*pb.Pages {{
-	// 		Root: map[string]*pb.Paths{
-	// 			"aavtic.dev": {
-	// 				IsEnd: false,
-	// 				Paths: map[string]*pb.Paths{
-	// 					"styles.css": { IsEnd: true, Paths: nil },
-	// 					"projects/ass_parser": {
-	// 						IsEnd: false,
-	// 						Paths: map[string]*pb.Paths{
-	// 							"scripts.js": { IsEnd: true, Paths: nil },
-	// 						},
-	// 					},
-	// 				},
-	// 			},
-	// 		},
-	// 	},
-	// 	{ 
-	// 		Root: map[string]*pb.Paths{
-	// 			"aavtic.dev/broler": {
-	// 				IsEnd: false,
-	// 				Paths: map[string]*pb.Paths{
-	// 					"styles.css": { IsEnd: true, Paths: nil },
-	// 					"projects/ass_parser": {
-	// 						IsEnd: false,
-	// 						Paths: map[string]*pb.Paths{
-	// 							"scripts.rs": { IsEnd: true, Paths: nil },
-	// 						},
-	// 					},
-	// 				},
-	// 			},
-	// 		},
-	// 	},
-	//
-	// 	{ 
-	// 		Root: map[string]*pb.Paths{
-	// 			"aavtic.dev/ttype": {
-	// 				IsEnd: false,
-	// 				Paths: map[string]*pb.Paths{
-	// 					"styles.css": { IsEnd: true, Paths: nil },
-	// 					"projects/ass_parser": {
-	// 						IsEnd: false,
-	// 						Paths: map[string]*pb.Paths{
-	// 							"scripts.py": { IsEnd: true, Paths: nil },
-	// 						},
-	// 					},
-	// 				},
-	// 			},
-	// 		},
-	// 	},
-	//
-	// }
+// pages := []*pb.Pages {{
+// 		Root: map[string]*pb.Paths{
+// 			"aavtic.dev": {
+// 				IsEnd: false,
+// 				Paths: map[string]*pb.Paths{
+// 					"styles.css": { IsEnd: true, Paths: nil },
+// 					"projects/ass_parser": {
+// 						IsEnd: false,
+// 						Paths: map[string]*pb.Paths{
+// 							"scripts.js": { IsEnd: true, Paths: nil },
+// 						},
+// 					},
+// 				},
+// 			},
+// 		},
+// 	},
+// 	{
+// 		Root: map[string]*pb.Paths{
+// 			"aavtic.dev/broler": {
+// 				IsEnd: false,
+// 				Paths: map[string]*pb.Paths{
+// 					"styles.css": { IsEnd: true, Paths: nil },
+// 					"projects/ass_parser": {
+// 						IsEnd: false,
+// 						Paths: map[string]*pb.Paths{
+// 							"scripts.rs": { IsEnd: true, Paths: nil },
+// 						},
+// 					},
+// 				},
+// 			},
+// 		},
+// 	},
+//
+// 	{
+// 		Root: map[string]*pb.Paths{
+// 			"aavtic.dev/ttype": {
+// 				IsEnd: false,
+// 				Paths: map[string]*pb.Paths{
+// 					"styles.css": { IsEnd: true, Paths: nil },
+// 					"projects/ass_parser": {
+// 						IsEnd: false,
+// 						Paths: map[string]*pb.Paths{
+// 							"scripts.py": { IsEnd: true, Paths: nil },
+// 						},
+// 					},
+// 				},
+// 			},
+// 		},
+// 	},
+//
+// }
