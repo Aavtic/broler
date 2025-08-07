a4851da2d71f8c33b9b371f76851e96e8123f527
Aadish M <89965681+Aavtic@users.noreply.github.com>
2025-08-08 00:11:21 +0530

    Added animation and styles for input #3

--- frontend/broler-frontend/app/page.tsx
+++ frontend/broler-frontend/app/page.tsx
@@ -1,87 +1,60 @@
 'use client';
 import { useState, useEffect } from 'react';
-import treeify from './utils/treeify';
 
 import  PageTree from './components/PageTree/PageTree';
 import { defaultTree } from '@/app/components/PageTree/PageTree';
 import { TreeViewItemB } from './utils/treeify';
 
 import { InputWithButton } from '@/components/input_field/InputWithButton';
-import AlertBox from '@/components/AlertBox/AlertBox'
+import GetPages from '@/app/utils/getPages'
 
 
+// interface onClickProps {
+//     url: string,
+// }
 
-interface onClickProps {
-    url: string,
-}
-async function sendReq({url} : onClickProps) {
-    const response = await sendRequest(url);
-    console.log(response)
-}
+// async function sendReq({url} : onClickProps) {
+//     const response = await sendRequest(url);
+//     console.log(response)
+// }
 
-function getTreeViewItemArray(json: any) {
-    return treeify(json)
-}
-
-async function sendRequest(request: string) {
-    const response =  await fetch('/api/live',
-        {
-            "method": "POST",
-            "headers": {
-                "Content-Type": "application/json",
-            },
-            "body": JSON.stringify({message: request})
-        }
-    );
-    return response.json()
-}
+// async function sendRequest(request: string) {
+//     const response =  await fetch('/api/live',
+//         {
+//             "method": "POST",
+//             "headers": {
+//                 "Content-Type": "application/json",
+//             },
+//             "body": JSON.stringify({message: request})
+//         }
+//     );
+//     return response.json()
+// }
 
 export default function Home() {
     const [data, setData] = useState<TreeViewItemB[]>(defaultTree);
     const [inputUrl, setInputUrl] = useState<string>('');
+    const [URL, setURL] = useState<string>('');
 
 
     function initiateNewSearch(result: "cancel" | "continue") {
         if (result === "cancel") return;
         if (result === "continue") {
-            console.log('initiating search')
-            sendReq({url: inputUrl})
+            setURL(inputUrl)
         }
     }
 
     useEffect(() => {
-        const eventSource = new EventSource('/api/live');
-        eventSource.onmessage = (event) => {
-            let done: boolean = false;
-            let parsed: any;
-            try {
-                parsed = JSON.parse(event.data);
-                console.log('json: ', JSON.stringify(parsed))
-                done = true;
-            } catch (e) {
-                console.error('Invalid JSON:', event.data);
-            }
+        console.log('in use effect');
+        if (inputUrl === '') return;
 
-            if (done) {
-                try {
-                    const array = getTreeViewItemArray(parsed);
-                    console.log(array);
-                    setData(array);
-                } catch(e) {
-                    console.error('Error while processing data: ', e);
-                }
-            }
-
-        };
-
-        eventSource.onerror = () => {
-            eventSource.close();
-        };
+        const cleanUp = GetPages(inputUrl, setData)
 
         return () => {
-            eventSource.close();
-        };
-    }, []);
+            cleanUp?.()
+        }
+
+    }, [URL]);
 
     const on_change = (e: any) => {
         setInputUrl(e.target.value)
--- frontend/broler-frontend/components/input_field/InputWithButton.tsx
+++ frontend/broler-frontend/components/input_field/InputWithButton.tsx
@@ -6,6 +6,8 @@ import AlertBox from '@/components/AlertBox/AlertBox'
 
 import { useState } from 'react';
 
+import { ShineBorder } from '@/components/magicui/shine-border'
+
 interface InputWithButtonProps {
     on_change: any,
     func: any,
@@ -15,10 +17,16 @@ export function InputWithButton({ on_change, func}: InputWithButtonProps ) {
     const [open, setOpen] = useState<boolean>(false);
   return (
     <div className="flex w-full max-w-sm items-center gap-2">
-      <Input type="url" placeholder="URL" onChange={on_change}/>
-      <Button type="submit" variant="outline" onClick={() => {setOpen(true)}}>
-      Start
-      </Button>
+    <div className="relative rounded-md">
+    <ShineBorder shineColor={["#A07CFE", "#FE8FB5", "#FFBE7B"]} duration={15}/>
+      <Input className="w-[300px]" type="url" placeholder="URL" onChange={on_change}/>
+    </div>
+    <div className="relative rounded-md">
+          <ShineBorder shineColor={["#eb2d66", "#5f3be3", "#FFBE7B"]} duration={15}/>
+          <Button type="submit" variant="outline" onClick={() => {setOpen(true)}}>
+          Start
+          </Button>
+    </div>
       <AlertBox open={open} onOpenChange={setOpen} func={func}/>
     </div>
   )
